67c70feb02361341240b6e516b2cc4945f665e91
Smehdii <siddik.mehdi@gmail.com>
2019-02-23 18:33:37 +0100

    Initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Button from "./Button";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Button />
+      </div>
+    );
+  }
+}
+
+export default App;
